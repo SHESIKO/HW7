fdab3105fd4b5eb2842cd99cf5c8a335d929355a
SHESIKO <favorit2021991@gmail.com>
2024-08-11 16:12:59 +0300

    Add solution

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        System.out.println("Введите количество ингредиентов которые у Вас есть");
+        System.out.println("Жабьи глаза");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Слёзы вурдалака");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Кости ворона");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Пельмени");
+        dumplingsCount = new Scanner(System.in).nextInt();
+        int zoro = 0;
+        int durability = 0;
+        int secrecy = 0;
+        int prohibitions = 0;
+
+        if ((toadEyesCount > 3 ) && (ravenBonesCount > 1 )) {
+            zoro++ ;
+            System.out.println("Вы можете приготовить Эликсир зоркости ");
+        }
+        if ((ravenBonesCount > 2) && (dumplingsCount >= 4)){
+            durability++;
+            System.out.println("Вы можете приготовть Эликсир стойкости ");
+        }
+        if((toadEyesCount >= 7) && (dumplingsCount >= 1) && (toadEyesCount >= 2)){
+            secrecy++;
+            System.out.println("Вы можете приготовить Эликсир скрытности");
+        }
+        if ((ghoulTearsCount >= 5) && (dumplingsCount >= 10) && (toadEyesCount >= 4) && (ravenBonesCount >=3)) {
+            prohibitions++;
+            System.out.println("Вы можете приготовить зпретный Эликсир");
+        }
+        if ((zoro < 1) && (durability < 1) && (secrecy < 1) && (prohibitions < 1)){
+            System.out.println("Вы не можете изготовить Эликсир");
+        }
+
+
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
 
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        int id = 2;
+        while (id <= 100)
+        {System.out.println("Уволен айтишник с id " + id);
+            id +=2;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*for (int id = 1; id <= 100; id++) {
+            if (id % 2 != 0) {
+                continue;
+            }else {
+                System.out.println("Уволен айтишник с ID "  +  id);
+            }
+        }*/
     }
 }
